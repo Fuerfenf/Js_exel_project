6bc9ad42902a0bfa0a3d98121c553eea5ba413ac
fuerfenf <egor602@mail.ru>
2020-08-05 18:08:09 +0300

    last update

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,13 @@
+export class Excel {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector) // marks $ DOM node
+        this.components = options.components || [] // in base free array
+    }
+    get
+
+
+    render() {
+        // // chanje elem  (afterbegin, afterend, beforeend, beforebegin)
+        // this.$el.insertAdjacentHTML('afterbegin',) //chanje in html
+    }
+}
---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,7 @@
+import {DOMLisener} from '@core/DOMLisener';
+
+export class ExcelComponent extends DOMLisener {
+    toHTML() { // return component pattern
+        return ''
+    }
+}
