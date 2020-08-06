8f5a112855e8b5e6f9f2ded7f5403d8b334770f3
fuerfenf <egor602@mail.ru>
2020-08-06 21:39:14 +0300

    last update frame

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,4 +1,44 @@
+// base const
+const CODES = {
+    A: 65,
+    Z: 90,
+};
 
-export function createTable() {
-    return '<h1>Table<h1>'
+
+function buildCell() {
+    return `<div class="cell" contenteditable=""></div>`;
+}
+function buildColumn(col) {
+    return `<div class="column">${col}</div>`;
+}
+function buildRow(content, num ) {
+    return `
+     <div class="row">
+        <div class="row-info">${num ? num : ''}</div>
+        <div class="row-data">${content}</div>
+     </div>
+    `;
+}
+
+function buildChr(el, index) {
+    return String.fromCharCode(CODES.A + index);
+}
+
+export function createTable(rCounter = 25) {
+    const clmCounter = CODES.Z - CODES.A + 1;
+    const rows = [];
+    const cols = new Array(clmCounter)
+        .fill('')
+        .map(buildChr)
+        .map(buildColumn)
+        .join('');
+    rows.push(buildRow(cols));
+    for (let i = 0; i <=rCounter; i++) {
+        const cell = new Array(clmCounter)
+            .fill('')
+            .map(buildCell)
+            .join('');
+        rows.push(buildRow(cell, i+1));
+    }
+    return rows.join('');
 }
